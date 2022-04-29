91fc070555612b482358f0dfa0ceba7d67313dc8
Juan David Parada Rodriguez <juand.paradar@utadeo.edu.co>
2022-04-28 23:41:50 -0500

    Enemy golpe

--- Assets/Sprites/Scripts/Enemies/Enemy1Controller.cs
+++ Assets/Sprites/Scripts/Enemies/Enemy1Controller.cs
@@ -5,54 +5,33 @@ using UnityEngine;
 
 public class Enemy1Controller : MonoBehaviour
 {
-    public int speed;
-    private bool moveRight;
+    
     private bool iscrushed;
     public Animator animator;
     private PlayerMovement Player;
+    private Enemy1Controller Ene;
     // Update is called once per frame
     void Start()
     {
         Player = FindObjectOfType<PlayerMovement>();
+        Ene = FindObjectOfType<Enemy1Controller>();
     }
-    void Update()
-    {
-        if (moveRight)
-        {
-            transform.Translate(-2 * Time.deltaTime * speed, 0, 0);
-        }
-        else
-        {
-            transform.Translate(2 * Time.deltaTime * speed, 0, 0);
-        }
-    }
-
+ 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "player")
         {
             Player.hit = true;
             iscrushed = true;
+            Ene.enabled= false;
             gameObject.GetComponent<BoxCollider2D>().enabled = false;
-            animator.SetBool("isCrush", iscrushed);
-            speed = 0;
+            animator.SetBool("die", iscrushed);
+           
             
             Invoke("dead", 1);
         }
 
-        if (collision.gameObject.CompareTag("Muro") || collision.gameObject.CompareTag("enemy"))
-        {
-            if (moveRight)
-            {
-                this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y);
-                moveRight = false;
-            }
-            else
-            {
-                this.transform.localScale = new Vector2(this.transform.localScale.x * -1, this.transform.localScale.y);
-                moveRight = true;
-            }
-        }
+      
     }
     private void dead()
     {
--- Assets/Sprites/Scripts/Player/PlayerMovement.cs
+++ Assets/Sprites/Scripts/Player/PlayerMovement.cs
@@ -2,29 +2,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Movement Parameters")]
     [SerializeField] private float speed;
     [SerializeField] private float jumpPower;
-
-    [Header("Coyote Time")]
-    [SerializeField] private float coyoteTime; //How much time the player can hang in the air before jumping
-    private float coyoteCounter; //How much time passed since the player ran off the edge
-
-    [Header("Multiple Jumps")]
-    [SerializeField] private int extraJumps;
-    private int jumpCounter;
-
-    [Header("Wall Jumping")]
-    [SerializeField] private float wallJumpX; //Horizontal wall jump force
-    [SerializeField] private float wallJumpY; //Vertical wall jump force
-
-    [Header("Layers")]
     [SerializeField] private LayerMask groundLayer;
     [SerializeField] private LayerMask wallLayer;
-
-    [Header("Sounds")]
-    [SerializeField] private AudioClip jumpSound;
-
     private Rigidbody2D body;
     private Animator anim;
     private BoxCollider2D boxCollider;
@@ -54,78 +35,52 @@ public class PlayerMovement : MonoBehaviour
         anim.SetBool("run", horizontalInput != 0);
         anim.SetBool("grounded", isGrounded());
 
-        //Jump
-        if (Input.GetKeyDown(KeyCode.Space))
-            Jump();
-
-        //Adjustable jump height
-        if (Input.GetKeyUp(KeyCode.Space) && body.velocity.y > 0)
-            body.velocity = new Vector2(body.velocity.x, body.velocity.y / 2);
-
-        if (hit)
+        //Wall jump logic
+        if (wallJumpCooldown > 0.2f)
         {
-            Rebote();
-            hit = false;
-        }
-
-        if (onWall())
-        {
-            body.gravityScale = 0;
-            body.velocity = Vector2.zero;
-        }
-        else
-        {
-            body.gravityScale = 7;
             body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
 
-            if (isGrounded())
+            if (onWall() && !isGrounded())
             {
-                coyoteCounter = coyoteTime; //Reset coyote counter when on the ground
-                jumpCounter = extraJumps; //Reset jump counter to extra jump value
+                body.gravityScale = 0;
+                body.velocity = Vector2.zero;
             }
             else
-                coyoteCounter -= Time.deltaTime; //Start decreasing coyote counter when not on the ground
+                body.gravityScale = 7;
+
+            if (Input.GetKey(KeyCode.Space))
+                Jump();
+            if (hit)
+            {
+                Rebote();
+                hit = false;
+            }
         }
+        else
+            wallJumpCooldown += Time.deltaTime;
     }
 
     private void Jump()
     {
-        if (coyoteCounter <= 0 && !onWall() && jumpCounter <= 0) return;
-        //If coyote counter is 0 or less and not on the wall and don't have any extra jumps don't do anything
-
-        SoundManager.instance.PlaySound(jumpSound);
-
-        if (onWall())
-            WallJump();
-        else
+        if (isGrounded())
         {
-            if (isGrounded())
-                body.velocity = new Vector2(body.velocity.x, jumpPower);
-            else
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+            anim.SetTrigger("jump");
+        }
+        else if (onWall() && !isGrounded())
+        {
+            if (horizontalInput == 0)
             {
-                //If not on the ground and coyote counter bigger than 0 do a normal jump
-                if (coyoteCounter > 0)
-                    body.velocity = new Vector2(body.velocity.x, jumpPower);
-                else
-                {
-                    if (jumpCounter > 0) //If we have extra jumps then jump and decrease the jump counter
-                    {
-                        body.velocity = new Vector2(body.velocity.x, jumpPower);
-                        jumpCounter--;
-                    }
-                }
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 10, 0);
+                transform.localScale = new Vector3(-Mathf.Sign(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
+            else
+                body.velocity = new Vector2(-Mathf.Sign(transform.localScale.x) * 3, 6);
 
-            //Reset coyote counter to 0 to avoid double jumps
-            coyoteCounter = 0;
+            wallJumpCooldown = 0;
         }
     }
 
-    private void WallJump()
-    {
-        body.AddForce(new Vector2(-Mathf.Sign(transform.localScale.x) * wallJumpX, wallJumpY));
-        wallJumpCooldown = 0;
-    }
 
     private bool isGrounded()
     {
@@ -142,12 +97,3 @@ public class PlayerMovement : MonoBehaviour
         body.velocity = new Vector3(body.velocity.x, +jumpPower, 0);
     }
 }
-
-
-
-
-
-
-
-
-
